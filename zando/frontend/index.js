c9b1e06ab418359739e7dd54912899dfb8b1d163
Kanik Mwav <kanikmwavlg@gmail.com>
2024-10-31 07:37:27 +0100

    nouvelle modif

--- zando/frontend/index.js
+++ zando/frontend/index.js
@@ -1,13 +1,153 @@
-let data = [];
-let filepaths = "data.json";
-fetch(filepaths)
-.then(response => response.json())
-.then(jsonData => {
-    data = jsonData;
+const data = [
+    {
+        "type": "text-image",
+        "image": "./img/white-headphones-airpods-black-background_1197797-216065.jpg",
+        "imageAlt": "wireless",
+        "content":"wireless",
+        "text": "<p class='card-title'><div class='b'>Wireless aerbud</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
 
-   
-})
-.catch(error => console.error('Erreur  lors du chargement des données :', error));
+    {
+        "type": "text-image",
+        "image": "./img/JPEG_20241029_132340_500437792799991550_converted.png",
+        "imageAlt": "Headphone",
+        "content":"Headphone",
+        "text": "<p class='card-title'><div class='b'>Headphone</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/images - 2024-10-29T125433.274.jpeg",
+        "imageAlt": "samsung",
+        "content":"samsung s24",
+        "text": "<p class='card-title'><div class='b'>Samsung S24</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/images - 2024-10-29T125541.105.jpeg",
+        "imageAlt": "tecno",
+        "content":"tecno",
+        "text": "<p class='card-title'><div class='b'>Tecno spark 20</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/images - 2024-10-29T125530.707.jpeg",
+        "imageAlt": "tecno",
+        "content":"tecno",
+        "text": "<p class='card-title'><div class='b'>Tecno</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/images - 2024-10-29T125444.206.jpeg",
+        "imageAlt": "samsung",
+        "content":"samsung s23",
+        "text": "<p class='card-title'><div class='b'>Samsun S23</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/3d-render-sleek-smartwatch-with-white-background_1205282-1997.jpg",
+        "imageAlt": "montre",
+        "content":"montre",
+        "text": "<p class='card-title'><div class='b'>Montre connectéé</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/71CtzAemsQL._AC_UF1000,1000_QL80_.jpg",
+        "imageAlt": "montre",
+        "content":"montre",
+        "text": "<p class='card-title'><div class='b'>Montre connectéé</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/71LlHaScSSL._AC_UF1000,1000_QL80_.jpg",
+        "imageAlt": "montre",
+        "content":"montre",
+        "text": "<p class='card-title'><div class='b'>Montre connectéé</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/PC-Portable-HP-Laptop-14-ep0043nf-14-Intel-Core-i7-16-Go-RAM-512-Go-D-Argent-naturel.jpg",
+        "imageAlt": "ordinateur",
+        "content":"ordinateur pc HP hp",
+        "text": "<p class='card-title'><div class='b'>Ordinateur HP</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/PC-Portable-HP-Laptop-14-ep0043nf-14-Intel-Core-i7-16-Go-RAM-512-Go-D-Argent-naturel.jpg",
+        "imageAlt": "ordinateur",
+        "content":"ordinateur pc lenovo Lenovo",
+        "text": "<p class='card-title'><div class='b'>Ordinateur Lenovo</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/JBLCharge5_Red_Main_2048x.png",
+        "imageAlt": "bluetooth",
+        "content":"bluetooth speaker",
+        "text": "<p class='card-title'><div class='b'>Bluetooth speaker</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/IMG-20241029-WA0010.jpg",
+        "imageAlt": "bluetooth",
+        "content":"bluetooth speaker",
+        "text": "<p class='card-title'><div class='b'>Bluetooth speaker</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/JBL_GO_4_3_4_LEFT_BLACK_48178_x1.png",
+        "imageAlt": "bluetooth",
+        "content":"bluetooth speaker",
+        "text": "<p class='card-title'><div class='b'>Bluetooth speaker</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/images - 2024-10-29T125319.635.jpeg",
+        "imageAlt": "iphone",
+        "content":"iphone 14 pro max",
+        "text": "<p class='card-title'><div class='b'>iPhone 14 pro max</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    },
+
+    {
+        "type": "text-image",
+        "image": "./img/apple-iphone-12-64gb-blue.jpg",
+        "imageAlt": "iphone",
+        "content":"iphone 13",
+        "text": "<p class='card-title'><div class='b'>iPhone 13</div></p><p class='card-text'>Toutes les marques sont disponibles<br><br>20 $</p>",
+        "buttonText": "Achetez le produit maintenant"
+    }
+
+
+
+    
+];
+
 
 function displayResults(items) {
     
@@ -20,17 +160,6 @@ function displayResults(items) {
         const blockDiv = document.createElement('div');
 
         blockDiv.classList.add('result-bloc');
-
-        if (item.video) {
-            const video = document.createElement('video');
-            video.src = item.video;
-            video.controls = true;
-
-            video.classList.add('mb-3');
-
-
-            blockDiv.appendChild(video);
-        }
 
         
         if (item.image) {
@@ -51,7 +180,7 @@ function displayResults(items) {
 
         const button = document.createElement('button');
 
-        button.classList.add('btn', 'btn-orangered');
+        button.classList.add('btn-orangered');
 
         button.innerText = item.buttonText;
 
@@ -79,3 +208,18 @@ function performSearch() {
 
 document.getElementById('searchInput').addEventListener('input', performSearch);
 
+
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const whatsappLink = document.getElementById('whatsappLink');
+
+    const message = "Bonjour, je veux acheter un produit.";
+
+    const phonenumber = "243975895900";
+
+    const whatsappUrl = 'https://wa.me/ ' + phonenumber + '?text=' + encodeURIComponent(message);
+
+    whatsappLink.href = whatsappUrl;
+});
